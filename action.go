6abd51dc177437d8783c1449b85f54bf2ef880b5
Christos Kotsis <kotsis.chr@gmail.com>
2020-10-30 02:28:21 +0200

    Renamed Try to Action. Generic callbacks, action callbacks

---
+++ action.go
@@ -0,0 +1,121 @@
+package uexec
+
+// Action holds the first return item
+type Action struct {
+	Values         []interface{}
+	CallBackFunc   interface{}
+	CallBackArgs   []interface{}
+	CallBackValues []interface{}
+	Err            interface{}
+}
+
+// AddCallBack adds a callback function that can run on Exec errors
+func (t Action) AddCallBack(callBackFunc interface{}, callBackArgs ...interface{}) Action {
+	t.CallBackFunc = callBackFunc
+	t.CallBackArgs = append(t.CallBackArgs, callBackArgs...)
+
+	return t
+}
+
+// CallBack function for running after an error has been caught
+func (t Action) CallBack() Action {
+	switch fn := t.CallBackFunc.(type) {
+	case func(...interface{}) []interface{}:
+		t.CallBackValues = fn(t.CallBackArgs...)
+	}
+	return t
+}
+
+// Get Returns a value
+func (t Action) Get(n int) interface{} {
+	return t.Values[n]
+}
+
+// Byte returns a value of type byte
+func (t Action) Byte(n int) byte {
+	return t.Values[n].(byte)
+}
+
+// ByteS returns an array of type byte
+func (t Action) ByteS(n int) []byte {
+	return t.Values[n].([]byte)
+}
+
+func (t Action) String(n int) string {
+	return t.Values[n].(string)
+}
+
+// StringS returns an array of strings
+func (t Action) StringS(n int) []string {
+	return t.Values[n].([]string)
+}
+
+// Int returns a value of type int
+func (t Action) Int(n int) int {
+	return t.Values[n].(int)
+}
+
+// Int8 returns a value of type int8
+func (t Action) Int8(n int) int8 {
+	return t.Values[n].(int8)
+}
+
+// Int16 returns a value of type int16
+func (t Action) Int16(n int) int16 {
+	return t.Values[n].(int16)
+}
+
+// Int32 returns a value of type int32
+func (t Action) Int32(n int) int32 {
+	return t.Values[n].(int32)
+}
+
+// Int64 returns a value of type int64
+func (t Action) Int64(n int) int64 {
+	return t.Values[n].(int64)
+}
+
+// IntS returns an array of type int
+func (t Action) IntS(n int) []int {
+	return t.Values[n].([]int)
+}
+
+// Int8S returns an array of type int8
+func (t Action) Int8S(n int) []int8 {
+	return t.Values[n].([]int8)
+}
+
+// Int16S returns an array of type int16
+func (t Action) Int16S(n int) []int16 {
+	return t.Values[n].([]int16)
+}
+
+// Int32S returns an array of type int32
+func (t Action) Int32S(n int) []int32 {
+	return t.Values[n].([]int32)
+}
+
+// Int64S returns an array of type int64
+func (t Action) Int64S(n int) []int64 {
+	return t.Values[n].([]int64)
+}
+
+// Float32 returns a value of type float32
+func (t Action) Float32(n int) float32 {
+	return t.Values[n].(float32)
+}
+
+// Float64 returns a value of type float64
+func (t Action) Float64(n int) float64 {
+	return t.Values[n].(float64)
+}
+
+// Float32S returns an array of type float32
+func (t Action) Float32S(n int) []float32 {
+	return t.Values[n].([]float32)
+}
+
+// Float64S returns an array of type float64
+func (t Action) Float64S(n int) []float64 {
+	return t.Values[n].([]float64)
+}
--- uexec.go
+++ uexec.go
@@ -6,71 +6,203 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var log = logrus.New()
+
+func init() {
+	log.SetFormatter(&logrus.JSONFormatter{})
+	log.SetLevel(logrus.InfoLevel)
+	log.SetReportCaller(false)
+}
+
 // ErrorHandler struct for handling errors
 type ErrorHandler struct {
-	Logger logrus.FieldLogger
-	Try    Try
+	Logger       logrus.FieldLogger
+	action       Action
+	callBackFunc interface{}
+	callBackArgs []interface{}
+	enableGCB    bool
+	elasticity   bool
+	onErAction   string
+	erP          int
 }
 
 // NewErrorHandler factory method to create a new errorhandler
 func NewErrorHandler() *ErrorHandler {
 	errorHandler := &ErrorHandler{
-		Logger: logrus.New(),
-		Try:    Try{},
+		Logger: log,
+		action: Action{},
+		erP:    -1,
 	}
+
 	return errorHandler
 }
 
-// Exec factory for creating a new try
-func (e *ErrorHandler) Exec(cmd ...interface{}) Try {
-	if len(cmd) == 0 {
-		return Try{}
-	}
+// AddGenericCallBack add a generic callback function that can run on Exec errors
+func (e *ErrorHandler) AddGenericCallBack(callBackFunc interface{}, callBackArgs ...interface{}) *ErrorHandler {
+	e.callBackFunc = callBackFunc
+	e.callBackArgs = callBackArgs
+	e.enableGenericCallBack(true)
+	log.WithFields(logrus.Fields{
+		"Action": "AddGenericCallBack",
+		"To":     "-",
+	}).Debug("Generic CallBack added successfully")
 
-	try := Try{}
-
-	var erP int = -1
-	for i, j := range cmd {
-		if isErr, err := try.getErr(j); isErr {
-			try.Err = append(try.Err, err)
-			erP = i
-		}
-
-	}
-
-	if erP >= 0 {
-		e.CheckE(try.Err[0])
-	}
-
-	for i, j := range cmd {
-		if i == erP {
-			continue
-		}
-		try.Values = append(try.Values, j)
-	}
-
-	return try
+	return e
 }
 
-// CheckE for checking the an error result and exiting if it is not nil
-func (e *ErrorHandler) CheckE(args interface{}) {
-	if args == nil {
+// DelGenericCallBack deletes the generic callback function that can run on Exec errors
+func (e *ErrorHandler) DelGenericCallBack() *ErrorHandler {
+	e.callBackFunc = nil
+	e.callBackArgs = nil
+	e.enableGenericCallBack(false)
+	log.WithFields(logrus.Fields{
+		"Action": "DelGenericCallBack",
+		"To":     "-",
+	}).Debug("Generic CallBack deleted successfully")
+
+	return e
+}
+
+// CallBack function for running after an error has been caught
+func (e *ErrorHandler) CallBack() *ErrorHandler {
+	switch fn := e.callBackFunc.(type) {
+	case func(...interface{}):
+		fn(e.callBackArgs)
+	}
+	return e
+}
+
+// enableGenericCallBack for enabling / disabling globacl callback function
+func (e *ErrorHandler) enableGenericCallBack(l bool) *ErrorHandler {
+	e.enableGCB = l
+	log.WithFields(logrus.Fields{
+		"Action": "EnableReportCaller",
+		"To":     l,
+	}).Debug("ReportCaller has been changed")
+	return e
+}
+
+// SetElasticity for changing the Elasticity of ErrorHandler
+func (e *ErrorHandler) SetElasticity(el bool) *ErrorHandler {
+	e.elasticity = el
+	log.WithFields(logrus.Fields{
+		"Action": "SetElasticity",
+		"To":     el,
+	}).Debug("SetElasticity has been changed")
+	return e
+}
+
+// SetLogLevel for chaning the loglevel of logrus
+func (e *ErrorHandler) SetLogLevel(l string) *ErrorHandler {
+	switch l {
+	case "info":
+		log.SetLevel(logrus.InfoLevel)
+	case "warn":
+		log.SetLevel(logrus.WarnLevel)
+	case "error":
+		log.SetLevel(logrus.ErrorLevel)
+	case "fatal":
+		log.SetLevel(logrus.FatalLevel)
+	case "debug":
+		log.SetLevel(logrus.DebugLevel)
+	default:
+		log.SetLevel(logrus.InfoLevel)
+	}
+	log.WithFields(logrus.Fields{
+		"Action": "SetLogLevel",
+		"To":     l,
+	}).Debug("LogLevel has been changed")
+	return e
+}
+
+// EnableReportCaller for enabling / disabling reportcaller of logrus
+func (e *ErrorHandler) EnableReportCaller(l bool) *ErrorHandler {
+	log.SetReportCaller(l)
+	log.WithFields(logrus.Fields{
+		"Action": "EnableReportCaller",
+		"To":     l,
+	}).Debug("ReportCaller has been changed")
+	return e
+}
+
+// ErP set explicit index of error in the Values []interface{}
+func (e *ErrorHandler) ErP(i int) *ErrorHandler {
+	e.erP = i
+	return e
+}
+
+// OnErr sets the action to be taken if Exec returns with error
+func (e *ErrorHandler) OnErr(a string) *ErrorHandler {
+	switch a {
+	case "exit":
+		e.onErAction = a
+	case "callback":
+		if !e.enableGCB {
+			e.onErAction = "exit"
+			log.WithFields(logrus.Fields{
+				"Action": "OnErr",
+				"To":     "exit",
+			}).Error("Has not been changed. Please add a callback function before you do this")
+			log.WithFields(logrus.Fields{
+				"Action": "OnErr",
+				"To":     "exit",
+			}).Error("OnErr is set to exit. Any error will cause a program exit 1")
+			return e
+		}
+		e.onErAction = a
+	default:
+		e.onErAction = "exit"
+	}
+	log.WithFields(logrus.Fields{
+		"Action": "OnErr",
+		"To":     a,
+	}).Debug("OnErr has been changed")
+	return e
+}
+
+// Exec factory for creating a new action
+func (e *ErrorHandler) Exec(cmd ...interface{}) Action {
+	e.action = Action{}
+
+	if len(cmd) == 0 {
+		return e.action
+	}
+
+	if len(cmd)-1 < e.erP {
+		log.WithFields(logrus.Fields{
+			"Action": "Exec",
+			"Status": "-",
+		}).Error("The Error Pointer, points to a none existant cmd index")
+		os.Exit(1)
+	}
+
+	e.action.CallBackArgs = append(e.action.CallBackArgs, cmd...)
+	e.getErr(cmd...)
+	e.getValues(cmd...)
+	e.checkE()
+
+	e.ErP(-1)
+	return e.action
+}
+
+func (e *ErrorHandler) getErr(cmd ...interface{}) {
+	if e.erP >= 0 {
+		e.action.Err = cmd[e.erP]
 		return
 	}
-	var err error = args.(error)
 
-	e.Logger.Error(err)
-	os.Exit(1)
+	for i, j := range cmd {
+		if isErr, err := e.ifErr(j); isErr {
+			e.action.Err = err
+			e.erP = i
+			return
+		}
+	}
+	e.action.Err = nil
 }
 
-// Try holds the first return item
-type Try struct {
-	Values       []interface{}
-	Err          []interface{}
-	optFunctions []func(...interface{}) []interface{}
-}
-
-func (t *Try) getErr(x interface{}) (bool, error) {
+// ifErr returns true/false and the error *if any* from an interface
+func (e *ErrorHandler) ifErr(x interface{}) (bool, error) {
 	switch x.(type) {
 	case error:
 		return true, x.(error)
@@ -79,96 +211,33 @@ func (t *Try) getErr(x interface{}) (bool, error) {
 	}
 }
 
-// Get Returns a value
-func (t Try) Get(n int) interface{} {
-	return t.Values[n]
+func (e *ErrorHandler) getValues(cmd ...interface{}) {
+	for i, j := range cmd {
+		if i == e.erP {
+			continue
+		}
+		e.action.Values = append(e.action.Values, j)
+	}
 }
 
-// Byte returns a value of type byte
-func (t Try) Byte(n int) byte {
-	return t.Values[n].(byte)
+// CheckE for checking the an error result and exiting if it is not nil
+func (e *ErrorHandler) checkE() {
+	if e.erP < 0 || e.action.Err == nil {
+		return
+	}
+
+	e.Logger.Error(e.action.Err)
+	e.errAction()
 }
 
-// ByteS returns an array of type byte
-func (t Try) ByteS(n int) []byte {
-	return t.Values[n].([]byte)
-}
+func (e *ErrorHandler) errAction() {
+	if e.onErAction == "exit" {
+		if !e.elasticity {
+			os.Exit(1)
+		}
+	}
 
-func (t Try) String(n int) string {
-	return t.Values[n].(string)
-}
-
-// StringS returns an array of strings
-func (t Try) StringS(n int) []string {
-	return t.Values[n].([]string)
-}
-
-// Int returns a value of type int
-func (t Try) Int(n int) int {
-	return t.Values[n].(int)
-}
-
-// Int8 returns a value of type int8
-func (t Try) Int8(n int) int8 {
-	return t.Values[n].(int8)
-}
-
-// Int16 returns a value of type int16
-func (t Try) Int16(n int) int16 {
-	return t.Values[n].(int16)
-}
-
-// Int32 returns a value of type int32
-func (t Try) Int32(n int) int32 {
-	return t.Values[n].(int32)
-}
-
-// Int64 returns a value of type int64
-func (t Try) Int64(n int) int64 {
-	return t.Values[n].(int64)
-}
-
-// IntS returns an array of type int
-func (t Try) IntS(n int) []int {
-	return t.Values[n].([]int)
-}
-
-// Int8S returns an array of type int8
-func (t Try) Int8S(n int) []int8 {
-	return t.Values[n].([]int8)
-}
-
-// Int16S returns an array of type int16
-func (t Try) Int16S(n int) []int16 {
-	return t.Values[n].([]int16)
-}
-
-// Int32S returns an array of type int32
-func (t Try) Int32S(n int) []int32 {
-	return t.Values[n].([]int32)
-}
-
-// Int64S returns an array of type int64
-func (t Try) Int64S(n int) []int64 {
-	return t.Values[n].([]int64)
-}
-
-// Float32 returns a value of type float32
-func (t Try) Float32(n int) float32 {
-	return t.Values[n].(float32)
-}
-
-// Float64 returns a value of type float64
-func (t Try) Float64(n int) float64 {
-	return t.Values[n].(float64)
-}
-
-// Float32S returns an array of type float32
-func (t Try) Float32S(n int) []float32 {
-	return t.Values[n].([]float32)
-}
-
-// Float64S returns an array of type float64
-func (t Try) Float64S(n int) []float64 {
-	return t.Values[n].([]float64)
+	if e.onErAction == "callback" {
+		e.CallBack()
+	}
 }
